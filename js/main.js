d8f6175d5c94808cc5620eebf4de74fbe1a95d69
Kevin Seguin <kevinseguin@gmail.com>
2013-06-11 16:25:34 -0400

    renderhomeView

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var app = {
 
+ renderHomeView: function() {
+    $('body').html(this.homeTpl());
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+},
+
 showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
@@ -9,22 +14,20 @@ showAlert: function (message, title) {
 },
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+    var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+    });
+},
 
     initialize: function() {
        var self = this;
 		this.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
+        //self.showAlert('Store Initialized', 'Info');
+		 self.renderHomeView();
+		 
+		 this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		 this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     });
     $('.search-key').on('keyup', $.proxy(this.findByName, this));
 }
