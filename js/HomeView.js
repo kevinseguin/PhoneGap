08624a720ec73824a82205e49d306e52ccba1cb7
Kevin Seguin <kevinseguin@gmail.com>
2013-06-12 10:48:37 -0400

    fixed homeview class

--- js/HomeView.js
+++ js/HomeView.js
@@ -4,21 +4,20 @@ this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
         this.el.on('keyup', '.search-key', this.findByName);
-    };
- 
-    this.initialize();
- 
-this.findByName = function() {
+    },
+	
+	this.findByName = function() {
     store.findByName($('.search-key').val(), function(employees) {
         $('.employee-list').html(HomeView.liTemplate(employees));
     });
-};
-
+},
 this.render = function() {
     this.el.html(HomeView.template());
     return this;
 };
-
+ 
+    this.initialize();
+ 
 }
 
 
