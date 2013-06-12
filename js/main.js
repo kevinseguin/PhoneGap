02b14fc9585c43463e2bf056eea4da5e28be652a
Kevin Seguin <kevinseguin@gmail.com>
2013-06-12 10:35:16 -0400

    homeview class

--- js/main.js
+++ js/main.js
@@ -1,39 +1,18 @@
 var app = {
-
- renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-},
-
+ 
 showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
     } else {
         alert(title ? (title + ": " + message) : message);
     }
-},
-
-    findByName: function() {
-    var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-},
-
+},  
     initialize: function() {
-       var self = this;
-		self.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
-		 
-		 self.homeTpl = Handlebars.compile($("#home-tpl").html());
-		 self.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-		 
-		 self.renderHomeView();
-		  
+    var self = this;
+    this.store = new MemoryStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
     });
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-}
-
+} 
 };
 
 app.initialize();
